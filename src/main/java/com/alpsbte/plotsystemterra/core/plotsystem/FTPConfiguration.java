b95427fda2b66c232dba3174ee8fb438f761114c
LordTuxn <r3tuxn@gmail.com>
2021-09-15 22:57:08 +0200

    Added compatibility for isSFTP column

--- src/main/java/com/alpsbte/plotsystemterra/core/plotsystem/FTPConfiguration.java
+++ src/main/java/com/alpsbte/plotsystemterra/core/plotsystem/FTPConfiguration.java
@@ -2,7 +2,6 @@ package com.alpsbte.plotsystemterra.core.plotsystem;
 
 import com.alpsbte.plotsystemterra.core.DatabaseConnection;
 
-import java.io.File;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -12,21 +11,23 @@ public class FTPConfiguration {
     private String schematicPath;
     private String address;
     private int port;
+    private boolean isSFTP;
     private String username;
     private String password;
 
     public FTPConfiguration(int ID) throws SQLException {
         this.ID = ID;
 
-        ResultSet rs = DatabaseConnection.createStatement("SELECT schematics_path, address, port, username, password FROM plotsystem_ftp_configurations WHERE id = ?")
+        ResultSet rs = DatabaseConnection.createStatement("SELECT schematics_path, address, port, isSFTP, username, password FROM plotsystem_ftp_configurations WHERE id = ?")
                 .setValue(this.ID).executeQuery();
 
         if (rs.next()) {
             this.schematicPath = rs.getString(1);
             this.address = rs.getString(2);
             this.port = rs.getInt(3);
-            this.username = rs.getString(4);
-            this.password = rs.getString(5);
+            this.isSFTP = rs.getBoolean(4);
+            this.username = rs.getString(5);
+            this.password = rs.getString(6);
         }
     }
 
@@ -48,6 +49,10 @@ public class FTPConfiguration {
 
     public int getPort() {
         return port;
+    }
+
+    public boolean isSFTP() {
+        return isSFTP;
     }
 
     public String getUsername() {
