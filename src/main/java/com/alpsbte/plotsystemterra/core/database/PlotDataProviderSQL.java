2ddbfed22f477112d93e87fc5541caf45bf55be9
Tintinkung <tintinkung.lemonade@gmail.com>
2025-07-06 08:08:46 +0700

    revert: "fix: missing initial plot_type"
    This reverts commit eb696b6f179d4cb9f626b33460ef1e5a63c9b55c.

--- src/main/java/com/alpsbte/plotsystemterra/core/database/PlotDataProviderSQL.java
+++ src/main/java/com/alpsbte/plotsystemterra/core/database/PlotDataProviderSQL.java
@@ -61,14 +61,13 @@ public class PlotDataProviderSQL implements PlotDataProvider {
             if (connection != null) {
                 connection.setAutoCommit(false);
 
-                try (PreparedStatement stmt = Objects.requireNonNull(connection).prepareStatement("INSERT INTO plot (city_project_id, difficulty_id, outline_bounds, initial_schematic, plot_version, plot_type, created_by)" +
-                        "VALUES (?, ?, ?, ?, (SELECT si.current_plot_version FROM system_info si WHERE system_id = 1), ?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+                try (PreparedStatement stmt = Objects.requireNonNull(connection).prepareStatement("INSERT INTO plot (city_project_id, difficulty_id, outline_bounds, initial_schematic, plot_version, created_by)" +
+                        "VALUES (?, ?, ?, ?, (SELECT si.current_plot_version FROM system_info si WHERE system_id = 1), ?)", Statement.RETURN_GENERATED_KEYS)) {
                     stmt.setString(1, cityProjectId);
                     stmt.setString(2, difficultyId);
                     stmt.setString(3, outlineBounds);
                     stmt.setBytes(4, initialSchematic);
-                    stmt.setInt(5, 1);
-                    stmt.setString(6, createPlayerUUID.toString());
+                    stmt.setString(5, createPlayerUUID.toString());
 
                     stmt.executeUpdate();
 
