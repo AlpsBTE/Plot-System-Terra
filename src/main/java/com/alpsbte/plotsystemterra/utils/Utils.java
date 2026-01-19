4cb365e3e07043b9353c4713ad62e200209fd517
Zoriot <zoriot@proton.me>
2026-01-19 21:12:26 +0100

    feat(database): ✨ refactor Utils class to use Lombok's UtilityClass and enhance method annotations

--- src/main/java/com/alpsbte/plotsystemterra/utils/Utils.java
+++ src/main/java/com/alpsbte/plotsystemterra/utils/Utils.java
@@ -3,32 +3,36 @@ package com.alpsbte.plotsystemterra.utils;
 import com.alpsbte.alpslib.utils.AlpsUtils;
 import com.alpsbte.alpslib.utils.head.AlpsHeadUtils;
 import com.alpsbte.alpslib.utils.item.ItemBuilder;
+import lombok.experimental.UtilityClass;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.jspecify.annotations.NonNull;
 
 import static net.kyori.adventure.text.format.NamedTextColor.*;
 
+@UtilityClass
 public class Utils {
+    @UtilityClass
     public static class ChatUtils {
+        private static Component infoPrefix;
+        private static Component alertPrefix;
+
         public static void setChatFormat(String infoPrefix, String alertPrefix) {
             ChatUtils.infoPrefix = AlpsUtils.deserialize(infoPrefix);
             ChatUtils.alertPrefix = AlpsUtils.deserialize(alertPrefix);
         }
 
-        private static Component infoPrefix;
-        private static Component alertPrefix;
-
-        public static Component getInfoFormat(Component infoComponent) {
+        public static @NonNull Component getInfoFormat(@NonNull Component infoComponent) {
             return infoPrefix.append(infoComponent.color(GREEN));
         }
 
-        public static Component getAlertFormat(Component alertComponent) {
+        public static @NonNull Component getAlertFormat(@NonNull Component alertComponent) {
             return alertPrefix.append(alertComponent.color(RED));
         }
     }
 
-    public static ItemStack getConfiguredItem(String material, String customModelData) {
+    public static ItemStack getConfiguredItem(@NonNull String material, String customModelData) {
         ItemStack base;
         if (material.startsWith("head(") && material.endsWith(")")) {
             String headId = material.substring(material.indexOf("(") + 1, material.lastIndexOf(")"));
